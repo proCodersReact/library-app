7006a0edea41b653624c618c0c2315c5ac547840
iSathsara <isuru.dex@gmail.com>
2021-01-29 03:56:09 +0530

    react-bootstrap added, Raleway font added, Welcome area completed.

---
+++ src/ClientApp.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReadingArea from "./components/ReadingArea";
+
+const ClientApp: React.FC = () => {
+    return(
+        <div>
+            <ReadingArea/>
+            <p>This is ClientApp.tsx. import other components here</p>
+        </div>
+
+    )
+};
+export default ClientApp;
+
