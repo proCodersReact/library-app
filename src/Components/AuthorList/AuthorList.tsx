b1471b2904e8fa30758c8154711b4f23f7a3941b
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-01-30 19:07:13 +0530

    author welcome with underline

--- src/Components/AuthorList/AuthorList.tsx
+++ src/Components/AuthorList/AuthorList.tsx
@@ -1,20 +1,15 @@
 import React, {Component} from 'react';
-import {Container} from 'react-bootstrap';
+import {Col, Container, Row} from 'react-bootstrap';
 
 class AuthorList extends Component{
     render(){
         return(
-            <div>
-                <Container>
-                    <h1 className={"text-left"}>Authors</h1><hr className={"bg-dark"}/>
-                    <ol>
-                        <li className={"text-left"}>Author 1 name</li>
-                        <li className={"text-left"}>Author 2 name</li>
-                        <li className={"text-left"}>Author 3 name</li>
-                    </ol>
-                    <button className={"ml-1"}>Add Author</button>
-                </Container>
-            </div>
+            <Row className={"authors"}>
+                <Col>
+                    <h2>Authors</h2>
+                    <hr className="author-title mt-2" />
+                </Col>
+            </Row>
         );
     }
 }
