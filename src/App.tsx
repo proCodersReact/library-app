74846162ea8ce7d2289435760450137c4c4ff0ee
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-01-30 14:16:12 +0530

    created AuthorList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import AuthorList from './Components/AuthorList/AuthorList'
 
 function App() {
   return (
     <div className="App">
       <h1> Library workspace </h1>
+        <AuthorList />
     </div>
   );
 }
---
+++ src/Components/AuthorList/AuthorList.tsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class AuthorList extends Component{
+    render(){
+        return(
+            <div>
+                <h5>hello this is from Author List Component</h5>
+            </div>
+        );
+    }
+}
+
+export default AuthorList;
