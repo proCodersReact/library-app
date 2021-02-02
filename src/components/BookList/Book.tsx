5e8cb1672f4b05988d03f88bfd9a2b8670f7cc14
unknown <jaffnacsc@gmail.com>
2021-02-02 19:43:58 +0530

    Book create and Book delete

--- src/components/BookList/Book.tsx
+++ src/components/BookList/Book.tsx
@@ -1,47 +1,95 @@
-import { Col, Row } from "react-bootstrap";
-import React from "react";
-import { Edit, Trash2 } from "react-feather";
+import { Col, Row, Container, Form, Button } from "react-bootstrap";
+import React, { useState } from 'react';
+import { Edit, Trash2, XCircle, Plus } from "react-feather";
 
-class Book extends React.Component<{}, any>{
-    constructor(props: string) {
-        super(props);
-        this.state = {
-            count: 0,
-            books: ['b1', 'b2', 'b3']
-        };
-        this.increase = this.increase.bind(this);
-        this.delete = this.delete.bind(this);
+
+
+const Book = () => {
+    const [books, setBooks] = useState(['gdg','shda']);
+    const [text, setText] = useState('');
+
+    
+
+    function createBooks() {
+        let newState = [...books, text];
+        setBooks(newState)
+        let t = ''
+        setText(t)
     }
 
-    increase() {
-        this.setState({ count: this.state.count + 1 });
+    function deleteBooks(name:string) {
+        let newState = books.filter((el: string) => el !== name);
+        setBooks(newState)
+       
     }
 
-    delete(id: string) {
-        this.setState((prevState: any) => ({
-            books: prevState.books.filter((el: string) => el !== id)
-        }));
-    }
-    render() {
-        if(this.state.books.length ===0){
-            return(<p>No books to show</p>)
-        }
-        else{
-            return (
-                this.state.books.map((book: string, index: number) =>
+    return (
+        <Container>
+            {
+                books.map((book: string, index: number) =>
                     <Row className='pl-1 pr-3 mt-1 book' key={index}>
                         <Col xs={8}  ><label >{index + 1}.{book}</label></Col>
                         <Col xs={4}>
                             <Row className='justify-content-end btn-options'>
                                 <Col xs={1} className='text-center text-warning clickBtn'><Edit size='20' /></Col>
-                                <Col xs={1} className='text-center text-danger clickBtn'><Trash2 size='20' onClick={() => this.delete(book)} /></Col>
+                                <Col xs={1} className='text-center text-danger clickBtn'><Trash2 size='20' 
+                                onClick={() => deleteBooks(book)}/></Col>
                             </Row>
                         </Col>
                     </Row>)
-            )
-        }
-       
-    }
+            }
+            <Row className='mx-0 mt-3 mb-4 add-btn'>
+                <Button variant='light' className="text-right p-0 flex-row"><Plus size={21} color='#234479' />   Add Book</Button>
+            </Row>
+            <Col className='p-0' sm={10}>
+                <Row className=' pb-1 mb-3 mx-1'>
+                    <Col sm={10}>
+                        <span className='add-book-title pt-2'>
+                            Create Book
+                            </span>
+                    </Col>
+                    <Col className='closeBtn text-right p-0' sm={2}>
+                        <XCircle color='#363636' className='mt-2 mr-3' />
+                    </Col>
+                </Row>
+
+
+                <Form className='mx-4'>
+                <Form.Group>
+                    <Form.Row>
+                        <Form.Label column="sm" lg={12} className='label'>
+                            Title of the Book
+                        </Form.Label>
+                        <Col sm={12}>
+                            <Form.Control size="sm" type="text" value={text} onChange={e => setText(e.target.value)}/>
+                        </Col>
+                        <Form.Label column="sm" lg={12} className='label'>
+                            ISBN
+                        </Form.Label>
+                        <Col sm={12}>
+                            <Form.Control size="sm" type="text"/>
+                        </Col>
+                        <Form.Label column="sm" lg={12} className='label'>
+                            Author
+                        </Form.Label>
+                        <Col sm={12}>
+                            <Form.Control size="sm" as="select">
+                            </Form.Control>
+                        </Col>
+                    </Form.Row>
+                </Form.Group>
+                <Col className='text-right mb-3 p-0' sm={12}>
+                    <Button variant="primary" size="sm" className='create-btn px-3' onClick={() => createBooks()}>
+                        Create
+                    </Button>
+                </Col>
+            </Form>
+
+
+                
+            </Col>
+        </Container>
+    )
 }
 
 export default Book;
