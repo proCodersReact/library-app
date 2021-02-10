b5fa4f36c8b6db22d1213578a324593a01782e1f
DinethShanG <gimhana.ds@gmail.com>
2021-02-10 22:13:09 +0530

    Resolve update button/create button change issue

--- src/components/BookList/Book.tsx
+++ src/components/BookList/Book.tsx
@@ -6,24 +6,18 @@ import {IAuthor} from "../../assets/types/LibraryTypes";
 type BookProps = {
     authors: IAuthor[]
 }
-
-type BookType= {
-    name:string,
-    isbn:string,
-    author:IAuthor,
-}
-
 const Book: React.FC<BookProps> = (Prop: any) => {
-
     const {authors} = Prop;
     var options = [];
     for (var i = 0; i < authors.length; i++) {
         options.push(<option key={i}>{authors[i].name}</option>);
     }
-    const [books, setBooks] = useState<BookType[]>([]);
-    const [name, setName] = useState<string>('');
-    const [ISBN, setISBN] = useState<string>('');
-    const [author, setAuthor] = useState<string>('');
+    const [books, setBooks] = useState<string[]>([]);
+    const [bookISBN, setBookISBN] = useState<string[]>([]);
+    const [bookAuthor, setBookAuthor] = useState<string[]>([]);
+    const [text, setText] = useState('');
+    const [ISBN, setISBN] = useState('');
+    const [author, setAuthor] = useState('testing authtp');
     const [index, setIndex] = useState(0);
     const [dispForm, setdispForm] = useState(false);
     const [bookName, setbookName] = useState(false);
@@ -32,10 +26,10 @@ const Book: React.FC<BookProps> = (Prop: any) => {
     const [validated, setValidated] = useState(false);
     const scrollDiv: React.MutableRefObject<any> = useRef()
 
-    const bookList = books.map((book: BookType, index: number) =>
+    const bookList = books.map((book: string, index: number) =>
         <Row className='p-0 mx-1 book' key={index}>
             <Col xs={9} className={'p-0'}>
-                <label>{index + 1}. {book.name}</label>
+                <label>{index + 1}.{book}</label>
             </Col>
             <Col xs={3} className={"text-right book-icon p-0"}>
                 <Edit className={"text-warning mr-2"} size='20' onClick={() => (updateBook(book))}/>
@@ -61,18 +55,23 @@ const Book: React.FC<BookProps> = (Prop: any) => {
         }
         else{
             setbookName(false)
-            const newBook:BookType = {name:name,isbn:ISBN,author:{name:author}}
             if(!bookUpdate){
-                let newState:BookType[] = [...books, newBook ];
-                setBooks(newState)
+                let newStateBooks:string[] = [...books, text];
+                let newStateISBN:string[] = [...bookISBN, ISBN];
+                let newStateAuthor:string[] = [...bookAuthor, author];
+                setBooks(newStateBooks)
+                setBookISBN(newStateISBN)
+                setBookAuthor(newStateAuthor)
                 let t = ' '
-                setName(t)
+                setText(t)
             }
             else{
-                books.splice(index , 1, newBook)
+                books.splice(index , 1, text)
+                bookISBN.splice(index , 1, ISBN)
+                bookAuthor.splice(index , 1, author)
             }
             let t = ''
-            setName(t)
+            setText(t)
             setISBN(t)
             setAuthor(t)
             setbookUpdate(false)
@@ -81,8 +80,8 @@ const Book: React.FC<BookProps> = (Prop: any) => {
         }
     }
 
-    function deleteBooks(book: BookType) {
-        let newState = books.filter((el: BookType) => el !== book);
+    function deleteBooks(name: string) {
+        let newState = books.filter((el: string) => el !== name);
         setBooks(newState)
 
     }
@@ -100,24 +99,34 @@ const Book: React.FC<BookProps> = (Prop: any) => {
     function showForm(s :boolean) {
         setValidated(false);
         setbookName(false)
-        if(bookUpdate){
-            setbookState('Update')
-        } else {
-            setbookState('Create')
-        }
+
         let newState = s
         setdispForm(newState)
         scrollDiv.current.scrollIntoView({behavior: 'smooth'})
     }
 
-    function updateBook(book: BookType) {
+    function updateBook(book: string) {
+        setbookState('Update')
+        let newState = book
         let index = books.indexOf(book)
+        let bookISBNValue = bookISBN[index]
+        let bookAuthorValue = bookAuthor[index]
         setIndex(index)
         setbookUpdate(true)
         showForm(true)
-        setName(books[index].name)
-        setISBN(books[index].isbn)
-        setAuthor(books[index].author.name)
+        setText(newState)
+        setISBN(bookISBNValue)
+        setAuthor(bookAuthorValue)
+    }
+
+    function addBook() {
+        setbookState('Create')
+        setbookUpdate(false)
+        showForm(true)
+        let t = ''
+        setText(t)
+        setISBN(t)
+        setAuthor(t)
     }
 
     return (
@@ -127,12 +136,12 @@ const Book: React.FC<BookProps> = (Prop: any) => {
             </Col>
 
             <Row className='mx-0 mt-3 mb-4 add-btn'>
-                <Button variant='light' className="text-right p-0 flex-row" onClick={() => showForm(true)}><Plus
+                <Button variant='light' className="text-right p-0 flex-row" onClick={() => addBook()}><Plus
                     size={21} color='#234479'/> Add Book</Button>
             </Row>
             <Col style={{display: dispForm ? 'inherit' : 'none'}} className='p-0' sm={10}>
                 <Row className=' pb-1 mb-3 mx-1'>
-                    <Col xs={10} className={'p-0'}>
+                    <Col xs={10}>
                         <span className='add-book-title pt-2'>
                             {bookState} Book
                             </span>
@@ -154,7 +163,7 @@ const Book: React.FC<BookProps> = (Prop: any) => {
                             </Col>
                             <Col sm={12}>
 
-                                <Form.Control className={'bookInput'} size="sm" type="text" value={name} onChange={e => setName(e.target.value)}
+                                <Form.Control className={'bookInput'} size="sm" type="text" value={text} onChange={e => setText(e.target.value)}
                                               required/>
 
                             </Col>
@@ -167,8 +176,8 @@ const Book: React.FC<BookProps> = (Prop: any) => {
                             <Form.Label column="sm" lg={12} className='label'>
                                 Author
                             </Form.Label>
-                            <Col sm={12} className='clear-in'>
-                                <Form.Control  size="sm" as="select" className={'bookInput bookSelect'} required value={author}  onChange={e => setAuthor(e.target.value)}>
+                            <Col sm={12}>
+                                <Form.Control size="sm" as="select" className={'bookInput bookSelect'} type="text" value={author} onChange={e => setAuthor(e.target.value)} required>
                                     <option value="" disabled selected hidden>Select an Author....</option>
                                     {options}
                                 </Form.Control>
